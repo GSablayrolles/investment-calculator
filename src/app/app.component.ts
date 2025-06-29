2827374ddce6ff7da4ede9c05553b3a264e70a99
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2025-06-29 16:20:18 +0200

    feat: displaying head for investment results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputsComponent } from './user-inputs/user-inputs.component';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
     selector: 'app-root',
-    imports: [HeaderComponent, UserInputsComponent],
+    imports: [HeaderComponent, UserInputsComponent, InvestmentResultsComponent],
     templateUrl: './app.component.html',
 })
 export class AppComponent { }
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { InvestmentResultsService } from './investment-results.service';
 
 @Component({
-  selector: 'app-investment-results',
-  imports: [],
-  templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+    selector: 'app-investment-results',
+    imports: [],
+    templateUrl: './investment-results.component.html',
+    styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
+    tableHeads = ["Year", "Investment Value", "Interest Year", "Total Interest", "Invested Capital"]
 
+    private investmentService = inject(InvestmentResultsService) // <=> constructor(...)
+
+
+
+    get investmentResults() {
+        return this.investmentService.getInvestmentResults();
+    }
 }
